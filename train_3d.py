f760ee6989a8cc1788967891e42e729b801504bd
athrunsunny <43680123+athrunsunny@users.noreply.github.com>
2026-03-17 22:24:15 +0800

    Update train_3d.py

--- train_3d.py
+++ train_3d.py
@@ -14,5 +14,18 @@ if __name__ == '__main__':
     results = model(r"E:\datasets\KITTI3D\training\image_2\000099.png", save=True, rect=False, imgsz=(384, 1280),
                     calib=r'E:\datasets\KITTI3D\training\calib\000099.txt')  # predict on an image
 
+    # KD
+    # # 加载剪枝后的模型权重文件
+    # teacher_model_path = r"yolov8m-pose-15.pt"
+    # student_model_path = "yolov8s-pose-15.pt"
+    # # student_model_path = r"./train_di_cwd/weights/last.pt"
+    # model_s = YOLO(student_model_path)
+    # model_t = YOLO(teacher_model_path)
+     
+    # distillation = model_t.model    # None
+    # loss_type = "at"   # mgd, cwd, skd, at, atm, pkd, dkd, qf
+
+    # model_s.train(distillation=distillation, loss_type=loss_type, **overrides)
+
 
 
